9a6bf7f5cb27bc7674adfc0856dc7713c9ae8904
hdennen <h@harrydennen.com>
2019-09-01 21:45:30 +0200

    add validation and caching

--- src/app.js
+++ src/app.js
@@ -1,34 +1,66 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const rp = require('request-promise-native');
-const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
-const endpoint = 'http://google.com';
+
+const port = process.env.PORT || 3000; // prod port needs to be set for node as environment variable
+const endpoint = 'https://betapistaging.hollywoodbets.net:443/api/punters/checkexistingclient?searchString=';
+
+const ipMap = new Map();
 
 function externalAuth(authToken) {
-    return rp(endpoint)
+    const request = endpoint+authToken;
+
+    options = {
+        url: request,
+        headers: {
+            Accept: 'application/json',
+        },
+        json: true
+    };
+
+    return rp(options)
 }
 
-function getAuthToken(req) {
-    return req;
+function validate(authRes) {
+    return authRes.responseMessage === 'Success';
+}
+
+function redirect(res) {
+    res.redirect('https://winningform-client.azurewebsites.net/');
+}
+
+function checkCache(ip) {
+    return ipMap.has(ip);
 }
 
 function checkUser(req, res, next) {
-    const filename = path.basename(req.url);
-    const extension = path.extname(filename);
-    console.log(`The file ${filename} was requested with extension ${extension}`);
+    // must put token on query '?authToken='
+    cachedValid = checkCache(req.ip);
 
-    const authToken = getAuthToken(req);
+    if (cachedValid) {
+        next();
+        return;
+    }
 
-    externalAuth(authToken)
-        .then(() => {
-            next();
+    externalAuth(req.query.authToken)
+        .then((authRes) => {
+            const isValid = validate(authRes);
+
+            if (isValid) {
+                ipMap.set(req.ip, 1);
+                next();
+            } else redirect(res);
         })
+        .catch((e) => {
+            console.dir(e)
+        });
 }
 
 app.use(checkUser);
 app.use(serveStatic('guide'));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.dir(`Listening on port ${port}`);
+    console.dir(`validation endpoint: ${endpoint}`);
+});
 
